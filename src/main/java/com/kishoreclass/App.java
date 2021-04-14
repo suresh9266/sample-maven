00a66ed3f5e8c5a28e9052abac30c6a94f1cb4a3
suresh <sureshroyal0432@gmail.com>
2021-04-14 20:26:44 +0000

    changes made again in source code

--- src/main/java/com/kishoreclass/App.java
+++ src/main/java/com/kishoreclass/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Again changes from cli" );
+        System.out.println( "Hello World! Again changes from cli A new change added in src code for testing github jenkins automation" );
     }
 }
