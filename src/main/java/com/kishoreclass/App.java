60a2555825beb894c964dcb8bc8ece426cdf6c54
suresh <sureshroyal0432@gmail.com>
2021-04-14 20:46:17 +0000

    final git-jenkins automation

--- src/main/java/com/kishoreclass/App.java
+++ src/main/java/com/kishoreclass/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Again changes from cli A new change added in src code for testing github jenkins automation" );
+        System.out.println( "Hello World! Again changes from cli A new change added in src code for testing github jenkins automation,And this is the final call stage to samole github jenkins automation" );
     }
 }
